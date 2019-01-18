8d23667b8b46fe67a2ab7351edb162fede2bbbfd
Oleg <olegi4.reg@gmail.com>
2019-01-18 13:05:24 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var string = `Hello
+!`;
+
+console.log(string);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone.name);
+
+console.log(persone["age"]);
+
+console.log(persone);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr);
+
+console.log(arr[3]);
+
+//alert("Hello World!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+let answer = promt("Do you have any many?", "Да");
+console.log(answer);
